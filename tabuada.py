860ef3cd93c92acb408bc29742255f333c37a509
Ribeiro-Ri <infotecnologuia@gmail.com>
2025-04-01 17:40:51 -0300

    mudou a tabuada para o usuario digitar

--- tabuada.py
+++ tabuada.py
@@ -1,4 +1,6 @@
+num = int(input("Digite um número para ver a tabuada: "))  # Solicita um número ao usuário
+
+print(f"\nTabuada do {num}:")
 for i in range(1, 11):  # Loop de 1 a 10
-    print(f"\nTabuada do {i}:")
-    for j in range(1, 11):  # Loop interno de 1 a 10
-        print(f"{i} × {j} = {i * j}")
+    print(f"{num} × {i} = {num * i}")
+
